e393199a63ce30d14f92d204ad7eb6f005b87cd5
EnzoSmidt <enzosmidt3000@gmail.com>
2022-07-04 10:46:45 -0300

    comecamoscsharp

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CursoGitHub.InterfaceConsole
+{ //início do namespace
+    internal class Program
+    { //início da classe
+        static void Main(string[] args)
+        { //início do método
+            /*
+             * Main - principal
+             * O método inicia-se com a letra maiúscula
+             * 
+             */
+
+            Console.WriteLine("Alô Mundo!");
+            Console.ReadKey();
+            
+        } //fim do método
+    } //fim da classe
+} //fim do namespace
